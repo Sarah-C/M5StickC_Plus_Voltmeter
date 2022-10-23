8bbefd5dfb18eb98ed662f6f89603a4121696134
Sarah Cartwright <sarah@untamed.zone>
2022-10-23 15:10:53 +0100

    Update VoltMeter2.ino
    Now with internal battery level !

--- VoltMeter2/VoltMeter2.ino
+++ VoltMeter2/VoltMeter2.ino
@@ -6,39 +6,58 @@ ADS1115 voltmeter;
 
 /*
 
-During testing I observed the following readings.
-
  Voltage = Raw read
  10.009836 = 20395
  10.009345 = 20394
+ Therefore, each single bit rise = 0.000491 volts
+ So meaningful digits are xx.xxx
 
-Therefore, each single bit rise translates to 0.000491 volts, 1 bit is half a millivolt, as described in the documentation.
-So meaningful digits are xx.xxx
-
-65535 (2 bytes of steps) * 0.000491 (each steps increase in volts) = 32.177685 volts.
-Therefore the highest accuracy reading of 3 decimal places, and up to 30 volts is seen by setting the gain simply to PAG_256
-(The circuit's not built to read higher volts? So it's not worth setting the gain differently for higher voltage, lower accuracy readings.)
+65535 (2 bytes) * 0.000491 = 32.177685 volts.
+Highest accuracy reading of 3 decimal places, and up to 30 volts = PAG_256
+(The circuit's not built to read higher volts? Not worth setting gain differently)
 
  */
 
-void setup() {
+float getBatteryLevel(void){
+  float emptyBatVoltage = 3.1f;
+  float fullBatVoltage = 4.15;
+  float vBat = M5.Axp.GetBatVoltage();
+  float batteryPercent = 100.0f * ((vBat - emptyBatVoltage) / (fullBatVoltage - emptyBatVoltage));
+  if(batteryPercent > 100) batteryPercent = 100;
+  if(batteryPercent < 0) batteryPercent = 0;
+  return batteryPercent;
+}
+
+void setup(){
   M5.begin();
   Wire.begin();
   voltmeter.setMode(SINGLESHOT);
   voltmeter.setRate(RATE_8); // Lowest most stable read time.
   voltmeter.setGain(PAG_256); // Most sensitive detection.
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextFont(2);
-  M5.Lcd.setTextSize(3);
   M5.Lcd.setRotation(3);
   M5.Lcd.setTextColor(WHITE, BLACK);
 }
 
-void loop(void) {
+void displayBattery(){
+  M5.Lcd.setCursor(30, 10);  
+  M5.Lcd.setTextFont(2);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.printf("Battery : %.1f %%   ", getBatteryLevel()); 
+}
+
+void displayVoltage(){
   double volts = 0;
-  volts = voltmeter.getValue() / 1000.0; // Convert pre-calibrated mV reading into volts.
-  if(volts < 0.000001) volts = 0.0; // Random noise causes negative values.
-  M5.Lcd.setCursor(30, 40);
+  volts = voltmeter.getValue() / 1000.0;
+  if(volts < 0.000001) volts = 0.0;
+  M5.Lcd.setCursor(30, 40);  
+  M5.Lcd.setTextFont(2);
+  M5.Lcd.setTextSize(3);
   M5.Lcd.printf("%.3lf v   ", volts); 
+}
+
+void loop(void){
+  displayVoltage();
+  displayBattery();
 }
 
